f3cb2661013526555d0420a571608ea07504d8c9
Assis Moraes <idd_fmoraes@uolinc.com>
2022-07-21 17:46:56 -0300

    Change port.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 docker container stop -t 0 aws-test
 docker container rm aws-test
 docker build -t aws-test:0.0.1 .
-docker run --name aws-test -p 8889:8080 aws-test:0.0.1
+docker run --name aws-test -p 80:8080 aws-test:0.0.1
